6bd0437fbf89abc54fb08c92783b24e5faddbdaa
yaojiejia <yaojiejia0715@gmail.com>
2025-03-21 11:09:09 -0400

    Add testing to questdb

--- test/sqlancer/dbms/TestQuestDBTLP.java
+++ test/sqlancer/dbms/TestQuestDBTLP.java
@@ -9,8 +9,13 @@ import sqlancer.Main;
 public class TestQuestDBTLP {
     @Test
     public void testQuestDB() {
-        assertEquals(0, Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
-                "--num-queries", "0", "questdb" }));
+        assertEquals(0,Main.executeMain(new String[] {
+            "--random-seed", "0",
+            "--timeout-seconds", TestConfig.SECONDS,
+            "--num-queries", "0",
+            "--num-threads", "1",     
+            "questdb"
+        }));
     }
     
 }
